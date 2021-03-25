3418eae308227cc3e2766ce0f4ce1a362c6c094b
Natenumber12 <71825153+Natenumber12@users.noreply.github.com>
2021-03-25 14:34:51 +0100

    It makes corners consistently!!

--- test2.m
+++ test2.m
@@ -4,15 +4,17 @@ clc;
 
 mask = single(zeros(120,160));
 out = 25;
-in = 6;
+in = 7;
 cone_angle = 15;
-for i = 1:360
+temp = [];
+for i = 0:90
     
-outter = int32(round(out*[cosd(i), sind(i)]));
-inner = int32(round(in*[cosd(i), sind(i)]));
+outter = round(out*[cosd(i), sind(i)]);
+inner = round(in*[cosd(i), sind(i)]);
 mask(60-outter(2),80+outter(1)) = outter(2);%+out*cosd(cone_angle);
 mask(60-inner(2),80+inner(1)) = outter(2);%+in*cosd(cone_angle);
-
+temp = [temp;inner(2)];
 end
-
+temp;
 mask(40:80,60:100)
+mask = mask + fliplr(mask);
